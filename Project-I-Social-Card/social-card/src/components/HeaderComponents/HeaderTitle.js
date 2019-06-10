ca45c35a4ec04673583c36122ceb5066bfeada65
MadisonViaLux <madison.s.mckown@gmail.com>
2019-06-10 16:01:57 -0700

    work thus far

--- Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderTitle.js
+++ Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderTitle.js
@@ -1,2 +1,15 @@
 import React from 'react';
 import './Header.css';
+
+
+const HeaderTitle = () => {
+    return (
+        <>
+            <header>Lambda School</header>
+
+            <date>@LambdaSchool * 26 jan</date>
+        </>
+    );
+};
+
+export default HeaderTitle;
