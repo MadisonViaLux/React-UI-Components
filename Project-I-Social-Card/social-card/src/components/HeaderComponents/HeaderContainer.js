a0d5bcdd4f33e581baa84c7680247ee8e8ae88d5
MadisonViaLux <madison.s.mckown@gmail.com>
2019-06-10 23:29:15 -0700

    progress

--- Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderContainer.js
+++ Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderContainer.js
@@ -3,3 +3,16 @@ import './Header.css';
 import HeaderContent from './HeaderContent';
 import HeaderTitle from './HeaderTitle';
 import ImageThumbnail from './ImageThumbnail';
+
+
+const HeaderContainer = () => {
+    return (
+        <div>
+        <ImageThumbnail />
+        <HeaderContent />
+        <HeaderTitle />
+        </div>
+    )
+}
+
+export default HeaderContainer;
